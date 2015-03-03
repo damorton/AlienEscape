c477daa366d0dcf71e0bf3212f4b1d5148f4b57b
David Morton <davidmortonmail@gmail.com>
2015-03-03 13:19:43 +0000

    updates

--- Alien Escape/Definitions.h
+++ Alien Escape/Definitions.h
@@ -1,14 +1,13 @@
 #ifndef DEFINITIONS_H_
 #define DEFINITIONS_H_
 
-//Screen dimension constants
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 #define XMLDOC "game.xml"
 #define SCREEN_FPS 60
 #define SCREEN_TICK_PER_FRAME (1000 / SCREEN_FPS)
 #define PADDING 5
-#define DEBUG 0
+#define DEBUG 1
 
 enum Gravity
 {
@@ -16,16 +15,14 @@ enum Gravity
 	GRAVITY_DOWN
 };
 
-//Game states
 enum GameStates
 {
 	STATE_NULL,
 	STATE_INTRO,
-	STATE_TITLE,
-	STATE_GREEN_OVERWORLD,
-	STATE_RED_ROOM,
-	STATE_BLUE_ROOM,
-	STATE_EXIT,
+	STATE_TITLE,	
+	STATE_PLAY,
+	STATE_PAUSE,
+	STATE_END,
 };
 
 #endif 
